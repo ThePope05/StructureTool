32e27fb45f1446034b409fb0e8539c6a33bb5650
ThePope05 <swobben05@gmail.com>
2023-12-27 20:48:30 +0100

    Added colors to terminal execute
    Php errors will also be projected in red

--- MyCommands/CreateProject.php
+++ MyCommands/CreateProject.php
@@ -1,79 +1,120 @@
 <?php
 
+define("TS_Reset", "\033[0m");
+
+$terminalColors = [
+    "red" => "\033[31m",
+    "green" => "\033[32m",
+    "yellow" => "\033[33m",
+    "blue" => "\033[34m",
+    "magenta" => "\033[35m",
+    "cyan" => "\033[36m",
+    "white" => "\033[37m",
+];
+
+$terminalStyles = [
+    "bold" => "\033[1m",
+    "underline" => "\033[4m",
+    "blink" => "\033[5m",
+    "reverse" => "\033[7m",
+    "hidden" => "\033[8m",
+];
+
+writeTerminalLine([["bold", "underline"], "magenta"], "Thank you for using my structure tool");
+
 if ($argc > 1) {
 
-    $allArg = scandir(__DIR__ . "/Structures");
-
-    array_shift($allArg);
-    array_shift($allArg);
-
-    if (in_array($argv[1], $allArg)) {
-        if ($argv[2] != null) {
-            echo "Creating structure" . "\n";
+    if (is_dir(__DIR__ . "/Structures/" . $argv[1])) {
+        if ($argc > 2) {
+            writeTerminalLine(["bold", "green"], "Creating project");
 
             if (is_dir(getcwd() . "/" . $argv[2])) {
-                echo "Project already exists" . "\n";
-                exit();
+                writeTerminalLine(["bold", "red"], "Project folder already exists");
+                cancelScript();
             }
 
             //Creating the project folder
-            
+
             $dir = getcwd() . "/" . $argv[2];
             $projectDir = mkdir($dir, 0777);
 
             if ($projectDir) {
-                echo "Created project folder" . "\n";
+                writeTerminalLine(["bold", "green"], "Created project folder");
                 scanCopyDir($dir, __DIR__ . "/Structures/" . $argv[1]);
             } else {
-                echo "Failed to create project folder" . "\n";
-                exit();
+                writeTerminalLine(["bold", "red"], "Failed to create project folder");
+                cancelScript();
             }
 
 
-            echo "\n" . "Process complete" . "\n \n";
+            writeTerminalLine(["bold", "green"], "\n Project created");
+            cancelScript();
         } else {
-            echo "Excpected [project name]" . "\n";
+            writeTerminalLine(["bold", "red"], "Expected project name");
+            cancelScript();
         }
-    }
-    else {
-        echo "Invalid keyword: " . $argv[1] . "\n \n";
-        echo "Valid keywords: " . "\n";
-        foreach($allArg as $arg){
-            echo "- " . $arg . "\n";
-        }
+    } else {
+        writeTerminalLine(["bold", "red"], "Project structure not found");
+        cancelScript();
     }
 } else {
-    echo "Excpected keyword" . "\n";
+    writeTerminalLine(["bold", "red"], "Expected project structure");
+    cancelScript();
 }
 
-function scanCopyDir($dir = "", $exampleDir = ""){
+function scanCopyDir($dir = "", $exampleDir = "")
+{
     $allFiles = scandir($exampleDir);
     $toScan = [];
 
-    foreach($allFiles as $file){
-        if($file != "." && $file != ".."){
-            if(!is_dir($exampleDir . "/" . $file)){
+    foreach ($allFiles as $file) {
+        if ($file != "." && $file != "..") {
+            if (!is_dir($exampleDir . "/" . $file)) {
                 $fileCopy = copy($exampleDir . "/" . $file, $dir . "/" . $file);
-                if($fileCopy){
-                    echo "Created " . $file . "\n";
+                if ($fileCopy) {
+                    writeTerminalLine(["bold", "green"], "Created " . $file);
+                } else {
+                    writeTerminalLine(["bold", "red"], "Failed to create " . $file);
+                    unlink($dir);
+                    cancelScript();
                 }
-                else{
-                    echo "Failed to create " . $file . "\n";
-                    exit();
-                }
-            }
-            else{
+            } else {
                 $newDir = mkdir($dir . "/" . $file, 0777);
-                if($newDir){
-                    echo "Created " . $file . "\n";
+                if ($newDir) {
+                    writeTerminalLine(["bold", "green"], "Created " . $file);
 
                     scanCopyDir($dir . "/" . $file . "/", $exampleDir . "/" . $file . "/");
-                }
-                else{
-                    echo "Failed to create " . $file . "\n";
-                    exit();
+                } else {
+                    writeTerminalLine(["bold", "red"], "Failed to create " . $file);
+                    unlink($dir);
+                    cancelScript();
                 }
             }
         }
     }
+}
+
+function writeTerminalLine($style = [["bold"], "red"], $text = "No Text Specified")
+{
+    global $terminalColors;
+    global $terminalStyles;
+
+    echo "\n";
+
+    if (!is_array($style[0])) {
+        $style[0] = [$style[0]];
+    }
+    foreach ($style[0] as $styleName) {
+        echo $terminalStyles[$styleName];
+    }
+
+
+    echo $terminalColors[$style[1]] . $text . "\n" . TS_Reset;
+    echo $terminalStyles["bold"] . $terminalColors["red"];
+}
+
+function cancelScript()
+{
+    echo TS_Reset . "\n";
+    exit();
 }
