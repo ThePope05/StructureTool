12eaa90af844a3e69735838e0b91b215d3a20ffb
ThePope05 <swobben05@gmail.com>
2024-02-05 10:24:38 +0100

    Added empty model

---
+++ MyCommands/Structures/mvc/app/models/HomePageModel.php
@@ -0,0 +1,24 @@
+<?php
+
+class HomePageModel
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    //Example of a query
+    public function index(int $id)
+    {
+        //Here you can write your query
+        $this->db->query("SELECT * FROM TABLE_NAME WHERE id = :id");
+
+        //Here you can bind your parameters
+        $this->db->bind(':id', $id);
+
+        //Here you can execute your query and return the result if you want
+        return $this->db->execute(true);
+    }
+}
